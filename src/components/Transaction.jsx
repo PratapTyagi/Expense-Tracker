81846b810607e5b2eebcf29e607ba195a3f822c0
Pratap Tyagi <tyagipratap111@gmail.com>
2021-04-08 12:40:26 +0530

    Delete transactions functionality

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { GlobalContext } from "../context/GlobalState";
 
 export const Transaction = ({ transactions }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+
   const sign = transactions.amount < 0 ? "-" : "+";
   return (
     <li className={transactions.amount < 0 ? "minus" : "plus"}>
@@ -9,7 +13,12 @@ export const Transaction = ({ transactions }) => {
         {sign}
         {Math.abs(transactions.amount)}
       </span>
-      <button className="delete-btn">x</button>
+      <button
+        onClick={() => deleteTransaction(transactions.id)}
+        className="delete-btn"
+      >
+        x
+      </button>
     </li>
   );
 };
